8a223386a3d5b6a1ada6c31295bf53b0f0a4c350
1U25CS032- Gunasekaran T <g8208003@gmail.com>
2026-08-25 20:17:22 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -12,13 +12,12 @@ CREATE TABLE Student (
 );
 desc Student;
 
-insert Student values("101","GUNA","2007-09-05","Male","1001");
+insert Student values("101","GUNA SEKARAN","2007-09-05","Male","1001");
 select * from Student;
 
 alter table Student add Email varchar(30);
 alter table Student add Phonenumber integer(10);
 desc Student;
-insert Student values("102", "GUNA","2006-09-05", "Male","10002", "g8208003@gmail.com", "7540061820");
+insert Student values("102", "GUNA SEKARAN","2006-09-05", "Male","10002", "g8208003@gmail.com", "7540061820");
 desc Student;
 select * from Student;
-
